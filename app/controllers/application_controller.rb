138ee743ea4cca5671db5ef46982b6d720508a5c
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2024-01-30 21:44:18 +0000

    added selenium driver

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,13 @@ require "selenium-webdriver"
 require 'nokogiri'
 require 'net/http'
 require 'uri'
+require 'fileutils'
 
 class ApplicationController < ActionController::API
     def test 
         # Capturing screenshots
         # puts "Called Endpoint"
-        # driver = Selenium::WebDriver.for :chrome
-        # driver.navigate.to "https://screenshotone.com"
-        # driver.save_screenshot("./screenshotone.png")
-        # driver.quit
+  
         
 
         # @doc = Nokogiri::XML(xml_str)
@@ -21,11 +19,29 @@ class ApplicationController < ActionController::API
 
         pages = []
         doc.xpath('//xmlns:url').each do |url|
-          pages.push(url.at_xpath('xmlns:loc').text)
+            pages.push(url.at_xpath('xmlns:loc').text)
         end
-        p pages.size
-        #parsed_data = Nokogiri::HTML.parse()
-        #puts parsed_data
 
+        pages.each_with_index do | item, index |
+            selenium(item)
+            if index > 2
+                break
+            end
+        end
+
+    end
+
+    def selenium(url)
+        directory_name = Time.now.strftime("%d-%m-%Y").to_s
+        if !Dir.exist?(directory_name)
+            FileUtils.mkdir_p directory_name
+        end
+
+        driver = Selenium::WebDriver.for :chrome
+        driver.navigate.to url
+        url = url.delete("/")
+        driver.save_screenshot("./#{directory_name}/#{url}.png")
+        driver.quit
+        # ##{url}/#{Time.now}/#{third_folder}/
     end
 end
