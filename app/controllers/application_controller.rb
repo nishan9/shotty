1fe65691309785765c607b57490559720c3813f7
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2024-01-30 20:50:27 +0000

    fetch list of urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
+require "selenium-webdriver"
+require 'nokogiri'
+require 'net/http'
+require 'uri'
+
 class ApplicationController < ActionController::API
+    def test 
+        # Capturing screenshots
+        # puts "Called Endpoint"
+        # driver = Selenium::WebDriver.for :chrome
+        # driver.navigate.to "https://screenshotone.com"
+        # driver.save_screenshot("./screenshotone.png")
+        # driver.quit
+        
+
+        # @doc = Nokogiri::XML(xml_str)
+
+        # puts @doc.xpath("//url")
+
+        doc = Nokogiri::XML(Net::HTTP.get(URI.parse('https://screenshotone.com/sitemap.xml')))
+
+        pages = []
+        doc.xpath('//xmlns:url').each do |url|
+          pages.push(url.at_xpath('xmlns:loc').text)
+        end
+        p pages.size
+        #parsed_data = Nokogiri::HTML.parse()
+        #puts parsed_data
+
+    end
 end
