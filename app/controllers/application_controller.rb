482bf519da3287b91e280641552ef0ea793a426d
nishan.mahamalimage2 <nishan.mahamalimage2@sky.uk>
2024-01-31 12:06:54 +0000

    added screen size mapper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,15 @@ class ApplicationController < ActionController::API
 
     def test 
 
+        required_sizes = ["iPhone XS", "Samsung S20"]
+        device_arr = JSON.load (File.open "./screens.json")
+
+        device_arr.each_with_index do |item, index |
+            if item["device"] == "iPhone XS"
+                p item["width"], item["height"]
+            end
+        end
+
         website_link = "https://screenshotone.com" + "/sitemap.xml"
         doc = Nokogiri::XML(Net::HTTP.get(URI.parse(website_link)))
 
@@ -24,6 +33,8 @@ class ApplicationController < ActionController::API
         end
     end
 
+
+
     def makedirs(url, browsers)
         arr = url.split("/")
         domain = arr[2]
@@ -38,26 +49,32 @@ class ApplicationController < ActionController::API
 
         current_time = Time.now.strftime("%d-%m-%Y").to_s
         
+
         browsers.each do | browser |
             single_dir = domain + "/" + browser + "/" + current_time + "/" + name
             FileUtils.mkdir_p single_dir unless Dir.exist?(single_dir)
-            selenium(url, single_dir, filename, browser)
+            selenium(url, single_dir, filename, browser, [[23,23],[232,2323],[232,23]])
         end
     end
 
 
-    def selenium(url, newdir, filename, browser)
-        if browser == "chrome"
-            driver = Selenium::WebDriver.for :chrome
-            driver.navigate.to url
-            driver.save_screenshot("./#{newdir}/#{filename}")
-            driver.quit
-        end
-        if browser == "firefox"
-            driver = Selenium::WebDriver.for :firefox
-            driver.navigate.to url
-            driver.save_screenshot("./#{newdir}/#{filename}")
-            driver.quit
+    def selenium(url, newdir, filename, browser, screens)
+        screens.each_with_index do | item, index |
+            if browser == "chrome"
+                driver = Selenium::WebDriver.for :chrome
+                driver.manage.window.resize_to(item[0], item[1])
+                driver.navigate.to url
+                driver.save_screenshot("./#{newdir}/#{filename}")
+                driver.quit
+            end
+            if browser == "firefox"
+                driver = Selenium::WebDriver.for :firefox
+                driver.manage.window.resize_to(item[0], item[1])
+                driver.navigate.to url
+                driver.save_screenshot("./#{newdir}/#{filename}")
+                driver.quit
+            end
+
         end
     end
 
